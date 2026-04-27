77b121b627de5254309cee11d0ee20fa10f9afd5
FerchoEL <lufeesle@gmail.com>
2026-04-27 17:52:40 -0600

    correccion permisos

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -12,6 +12,7 @@ use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Database\Eloquent\Model;
 use Spatie\Permission\Models\Role;
 use Illuminate\Support\Facades\Hash;
 
